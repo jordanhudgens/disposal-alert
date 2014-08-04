94947c30f7010aff3179589cd5aaa120ce9b6f7a
Jordan Hudgens <jordan@wow.com>
2014-08-04 16:50:42 -0700

    initial app build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Disposalalert::Application.config.secret_key_base = '4e98119800fd0e2337c96cf000bb096ee25d4e19d5cae834ee638d7cc252e8af2f7af4e737409b8795498af07ab7af8561b677af6e6c76317d3988a39058e5c5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Disposalalert::Application.config.session_store :cookie_store, key: '_disposalalert_session'
