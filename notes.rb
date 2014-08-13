929724a0a7a8dbbcbe510ee9c485367e52ffb0c8
Jordan Hudgens <jordan@wow.com>
2014-08-13 05:20:12 -0500

    added todos

--- notes.rb
+++ notes.rb
@@ -1,3 +1,8 @@
 # TODO Create email send function
 # TODO Integrate ActiveAdmin
 # TODO after_create in model, after_update remove items
+# TODO Views/controller for recurring alerts
+# TODO Integrate call to recurring alerts to d alerts
+# TODO Have method create alert in the future, make sure to check for duplicates
+# TODO Integrate bootstrap
+# TODO Integrate Rspec to test potential cases
