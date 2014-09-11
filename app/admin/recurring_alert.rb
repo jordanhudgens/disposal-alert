8f9ece7f04b49b9351359fb4874fcfc0c0e613c3
Jordan Hudgens <jordan@wow.com>
2014-09-11 04:42:45 -0500

    integrated fix for the active admin form for the recurring alerts

--- app/admin/recurring_alert.rb
+++ app/admin/recurring_alert.rb
@@ -5,11 +5,14 @@ ActiveAdmin.register RecurringAlert do
   INTERVALS = %w(Weekly Twice-Monthly Monthly Quarterly Annually)
 
   form do |f|
-    f.input :name
-    f.input :alert_category
-    f.input :contact_name
-    f.input :alert_interval, :as => :select, :collection => INTERVALS
-    f.input :start_date, :as => :datepicker
+    f.inputs "Test" do
+      f.input :name
+      f.input :alert_category
+      f.input :contact_name
+      f.input :alert_interval, :as => :select, :collection => INTERVALS
+      f.input :start_date, :as => :datepicker
+    end
+    f.actions
   end
 
 end
